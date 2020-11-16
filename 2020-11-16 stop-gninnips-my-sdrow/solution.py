42f16cd35984ce60813c88fd3d59edbb062818b1
ItsTheCreeper <rclark200618@gmail.com>
2020-11-16 22:02:59 +0800

    Added Python solution to "Stop gninnpS My sdroW!"

---
+++ 2020-11-16 stop-gninnips-my-sdrow/solution.py
@@ -0,0 +1,18 @@
+# solution to "Stop gninnipS My sdroW!"
+def spin_words(sentence):
+    # "spin": input is split into a list of words
+    # "output": output stream
+    spin = sentence.split(" ")
+    output = ""
+
+    # add reversed word if length >= 5
+    # otherwise add normal word
+    for word in spin:
+        if len(word) >= 5:
+            output += word[::-1]
+        else:
+            output += word
+
+        output += " "
+        
+    return output[:-1]
